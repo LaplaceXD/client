54e5635a86cd7538d0404f030f12aafe1e74567b
Laplace <johnalexisbuot@gmail.com>
2021-07-08 19:04:06 +0800

    Initialized breakpoints for styling

---
+++ src/chakraUI/breakpoints.ts
@@ -0,0 +1,9 @@
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
+export default createBreakpoints({
+    sm: "360px",
+    md: "412px",
+    lg: "768px",
+    xl: "1280px",
+    "2xl": "2160px",
+});
